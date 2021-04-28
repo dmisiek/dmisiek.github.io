e6d8034e84a3bf998f7434d1f2aa90d775fe17f3
dmisiek <dominikmisiek02@gmail.com>
2021-04-28 14:51:00 +0200

    update v1.02

---
+++ websites/dm-lib/script.js
@@ -0,0 +1,4 @@
+var dmNavbar1Ham = document.getElementById("dm-navbar1Ham");
+dmNavbar1Ham.addEventListener("click", ()=>{
+    document.getElementById("dm-navbar1MobileLinksDiv").classList.toggle("dm-transformX0");
+})
