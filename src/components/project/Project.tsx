65c6579e1dc6c830834008aca4b35cd4533ad406
dmisiek <dominikmisiek02@gmail.com>
2022-02-08 11:08:18 +0100

    Project component completed

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -1,23 +1,58 @@
 import './Project.scss';
-import DBS1 from '../../media/images/dbs1.png';
-import DBS2 from '../../media/images/dbs2.png';
-import DBS3 from '../../media/images/dbs3.png';
 
-const Project = function(){
+import IconLINK from '../../media/icons/iconLINK.png';
+import IconREACT from '../../media/icons/iconREACT.png';
+import IconCSS from '../../media/icons/iconCSS.png';
+import IconJS from '../../media/icons/iconJS.png';
+import IconHTML from '../../media/icons/iconHTML.png';
+
+interface props{
+  img1: string;
+  img2: string;
+  img3: string;
+  title: string;
+  desc: string;
+  to: string;
+  tags?: Array<string>;
+}
+
+
+
+const Project = function(props:props){
+
+  const getIcon = (name: string) => {
+    switch(name as any){
+      case "REACT": return IconREACT;
+      case "CSS": return IconCSS;
+      case "JS": return IconJS;
+      case "HTML": return IconHTML;
+      default: return "";
+    }
+  }
 
   return(
     <div className='project'>
 
       <div className='project_grid'>
         <div className='project_grid_content'>
-          <h1>DEADBYSTATS.EU</h1>
-          <p>Fanlike project that allows players of Dead by Daylight to check & compare their stats and save they progress.</p>
-        
+          <div>
+            <a href={props.to} target="_blank" className='title'>{props.title} <img src={IconLINK} alt="LINK" /></a>
+            <p className='desc'>{props.desc}</p>
+          </div>
+
+          <div className='project_grid_content_tags'>
+            {
+              props.tags?.map( tag => {
+
+                return <h1 className='tag'><img src={getIcon(tag)} alt={tag} />{tag}</h1>
+              })
+            }
+          </div>
         </div>
         <div className='project_grid_images'>
-          <img src={DBS1} alt="dbs1" />
-          <img src={DBS2} alt="dbs2" />
-          <img src={DBS3} alt="dbs3" />
+          <img src={props.img1} alt="dbs1" />
+          <img src={props.img2} alt="dbs2" />
+          <img src={props.img3} alt="dbs3" />
         </div>
 
       </div>
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -4,6 +4,15 @@ import HeroImage from '../../media/images/hero.png'
 import Footer from '../../components/footer/Footer'
 import Project from '../../components/project/Project'
 
+// Project images
+import DBS1 from '../../media/images/dbs1.png';
+import DBS2 from '../../media/images/dbs2.png';
+import DBS3 from '../../media/images/dbs3.png';
+import SZKLARZ1 from '../../media/images/szklarz1.png';
+import SZKLARZ2 from '../../media/images/szklarz2.png';
+import SZKLARZ3 from '../../media/images/szklarz3.png';
+
+
 const Home = function(){
   return(
     <>
@@ -30,7 +39,18 @@ const Home = function(){
       </div>
       <div className='h100'>
 
-        <Project />
+        <Project img1={DBS1} img2={DBS2} img3={DBS3} 
+          to={"https://deadbystats.eu"}
+          title={"DEADBYSTATS.EU"}
+          desc={"Fanlike project that allows players of Dead by Daylight to check & compare their stats and save they progress. I created frontend only."}
+          tags={["REACT", "CSS"]}
+        />
+        <Project img1={SZKLARZ1} img2={SZKLARZ2} img3={SZKLARZ3} 
+          to={"https://szklarzratajczyk.pl"}
+          title={"SZKLARZ RATAJCZYK.PL"}
+          desc={"Commercial website to local company. It includes their services, contact, location and photos from their realizaiotns. I used Firebase for backend here."}
+          tags={["REACT", "CSS"]}
+        />
 
       </div>
       <Footer />
